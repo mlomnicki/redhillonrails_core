a0fa51f43ac07635c86e48bbf315cb0c27b20a21
Michał Łomnicki <michal.lomnicki@gmail.com>
2011-07-20 00:01:30 +0200

    just check if Mysql2IndexDefinition is defined (#11)

--- lib/redhillonrails_core/active_record/connection_adapters/mysql_adapter_fixes.rb
+++ lib/redhillonrails_core/active_record/connection_adapters/mysql_adapter_fixes.rb
@@ -3,19 +3,15 @@ module RedhillonrailsCore
     module ConnectionAdapters
       module MysqlAdapterFixes
 
-        # Mysql2 gem adds own Mysql2IndexDefinition in versions from 0.2.7 to 0.2.11.
-        # We must include RedhillonrailsCore IndexDefinition to support
-        # case sensitivness.
-        # 0.3 line of mysql2 removes it again so hopefully we'll remove that
-        # obsure piece of code too.
+        # Starting from mysql2 0.2.7 Mysql2IndexDefinition is used.
+        # In 0.2 line it is inculuded in gem itself.
+        # Starting from version 0.3 it has gone to active_record mysql adapter
+        # We have to additionally monkey patch it.
         def self.included(base)
           super
-          if defined?(Mysql2::VERSION)
-            major, minor, patch = Mysql2::VERSION.split('.')
-            if minor.to_i == 2 && patch.to_i >= 7
-              ::ActiveRecord::ConnectionAdapters::Mysql2IndexDefinition.class_eval do
-                include ::RedhillonrailsCore::ActiveRecord::ConnectionAdapters::IndexDefinition
-              end
+          if defined?(::ActiveRecord::ConnectionAdapters::Mysql2IndexDefinition)
+            ::ActiveRecord::ConnectionAdapters::Mysql2IndexDefinition.class_eval do
+              include ::RedhillonrailsCore::ActiveRecord::ConnectionAdapters::IndexDefinition
             end
           end
         end
