102d29ca8a22e071240ab087bffbdcb51539f825
Michał Łomnicki <michal.lomnicki@gmail.com>
2011-07-20 03:19:37 +0200

    column.case_sensitive and column.unique are deprecated

--- lib/redhillonrails_core/active_record/connection_adapters/column.rb
+++ lib/redhillonrails_core/active_record/connection_adapters/column.rb
@@ -2,11 +2,23 @@ module RedhillonrailsCore
   module ActiveRecord
     module ConnectionAdapters
       module Column
-        attr_accessor :unique_scope
-        attr_accessor :case_sensitive
-        alias case_sensitive? case_sensitive
+        attr_writer :unique_scope
+        attr_writer :case_sensitive
+
+        def unique_scope
+          ActiveSupport::Deprecation.warn "[redhillonrails_core] column.unique_scope is deprecated and will be removed in 2.0"
+          @unique_scope
+        end
+
+        def case_sensitive
+          ActiveSupport::Deprecation.warn "[redhillonrails_core] column.case_sensitive is deprecated and will be removed in 2.0"
+          @case_sensitive
+        end
+
+        alias :case_sensitive? :case_sensitive
 
         def unique?
+          ActiveSupport::Deprecation.warn "[redhillonrails_core] column.unique? is deprecated and will be removed in 2.0"
           !unique_scope.nil?
         end
 
